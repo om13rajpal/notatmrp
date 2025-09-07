973d3a480b4fa09e9395d26e2571f19c15c1e0c5
om rajpal <omrajpal.exe@gmail.com>
2025-09-07 15:14:17 +0530

    added swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import { NODE_ENV } from "./config/env.config";
 import compression from "compression";
 import cors from "cors";
 import helmet from "helmet";
+import swaggerUI from 'swagger-ui-express'
+import swaggerDocument from "../swagger.json";
+
 import {
   internalServerError,
   pageNotFound,
@@ -25,6 +28,8 @@ app.use(cors());
 app.use(helmet());
 
 app.use(express.json());
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/", authRouter);
 app.use("/products", productRouter);
